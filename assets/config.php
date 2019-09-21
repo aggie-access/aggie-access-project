feaa022512aa8d5709b59287c743d2599674899c
Carson Calcutt <46988440+carsoncalcutt@users.noreply.github.com>
2019-09-21 02:29:09 -0400

    Update config.php

--- assets/config.php
+++ assets/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $server_username = "fg31igihj6wh";
 $server_password = "fjN*70v5s";
-$dbname = "aggie_access";
+$dbname = "aggie_access_";
 
 $conn = new mysqli($servername, $server_username, $server_password, $dbname);
 if ($conn->connect_error) {
