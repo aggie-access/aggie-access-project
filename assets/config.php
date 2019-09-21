491c92fd4e5db369ff79953b5c08d13e30ccf7b0
Carson Calcutt <46988440+carsoncalcutt@users.noreply.github.com>
2019-09-20 23:15:08 -0400

    Update config.php

--- assets/config.php
+++ assets/config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql300.epizy.com";
-$server_username = "epiz_24469068";
-$server_password = "2gpwL6TuTXfO2K";
-$dbname = "epiz_24469068_aggie_access";
+$servername = "aggieaccessproject.co";
+$server_username = "fg31igihj6wh";
+$server_password = "fjN*70v5s";
+$dbname = "aggie_access";
 
 $conn = new mysqli($servername, $server_username, $server_password, $dbname);
 if ($conn->connect_error) {
