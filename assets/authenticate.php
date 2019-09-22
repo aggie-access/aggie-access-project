93cb9d26009bad1696f8b792ea2b7bddda949538
Carson Calcutt <cmcalcutt@aggies.ncat.edu>
2019-09-22 04:01:32 -0400

    Update authenticate.php

--- assets/authenticate.php
+++ assets/authenticate.php
@@ -15,28 +15,28 @@ if ($stmt = $conn->prepare('SELECT banner_id, password FROM users WHERE banner_i
 	$stmt->store_result();
 
 	if ($stmt->num_rows > 0) {
-	    $stmt->bind_result($username, $password);
-	    $stmt->fetch();
-	    if ($_POST['password'] === $password) {
-	        session_regenerate_id();
-	        $_SESSION['loggedin'] = TRUE;
-	        $_SESSION['username'] = $_POST['username'];
+		$stmt->bind_result($username, $password);
+		$stmt->fetch();
+		if ($_POST['password'] === $password) {
+			session_regenerate_id();
+			$_SESSION['loggedin'] = TRUE;
+			$_SESSION['username'] = $_POST['username'];
 
-	        $banner_id=$_POST['username'];
+			$banner_id=$_POST['username'];
 
-	        $sql="SELECT first_name FROM student WHERE banner_id='$banner_id'";
-	        $result = $conn->query($sql);
-	        $row = $result->fetch_assoc();
+			$sql="SELECT first_name FROM student WHERE banner_id='$banner_id'";
+			$result = $conn->query($sql);
+			$row = $result->fetch_assoc();
 
-	        $_SESSION['name'] = $row["first_name"];
+			$_SESSION['name'] = $row['first_name'];
 
-	        header('Location: ../dashboard.php');
-	    } else {
-	        header('Location: ../index.php?error=1');
-	    }
+			header('Location: ../dashboard.php');
+		} else {
+			header('Location: ../index.php?error=1');
+		}
 
 	} else {
-	    header('Location: ../index.php?error=1');
+		header('Location: ../index.php?error=1');
 	}
 	$stmt->close();
 }
