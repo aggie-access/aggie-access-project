3b65234b3698d149d3a16e76df7e974bbebb18c0
Carson Calcutt <cmcalcutt@aggies.ncat.edu>
2019-09-30 04:10:16 -0400

    Update student dashboard file name and links

--- assets/navbar.php
+++ assets/navbar.php
@@ -6,7 +6,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="dashboard.php"><img src="images/logo.png" class="navbar-logo"></a>
+			<a class="navbar-brand" href="student-dashboard.php"><img src="images/logo.png" class="navbar-logo"></a>
 		</div>
 		<ul class="nav navbar-nav navbar-right">
 			<li><a href="assets/logout.php" class="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
