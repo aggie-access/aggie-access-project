c24e707032d30f7c98555b0ed8d48f9abd287b95
Carson Calcutt <46988440+carsoncalcutt@users.noreply.github.com>
2019-09-30 08:34:27 -0400

    Update academic-transcript.php

--- academic-transcript.php
+++ academic-transcript.php
@@ -59,13 +59,13 @@ $gpa=$quality_points/$earned_hours;
     <p style="margin-bottom:35px;">Below is an unofficial copy of your current academic transcript.</p>
 
     <div class="row row-no-gutters transcript-grid first-row">
-      <div class="col-xs-2">
+      <div class="col-xs-3">
         <strong>Course</strong>
       </div>
       <div class="col-xs-4">
         <strong>Title</strong>
       </div>
-      <div class="col-xs-1">
+      <div class="col-xs-2">
         <strong>Credits</strong>
       </div>
       <div class="col-xs-2 mobile-hide">
@@ -74,20 +74,16 @@ $gpa=$quality_points/$earned_hours;
       <div class="col-xs-1">
         <strong>Grade</strong>
       </div>
-      <div class="col-xs-2 mobile-hide">
-        <strong>Quality Points</strong>
-      </div>
     </div>
 
     <?php
     while($row_transcript = $result_transcript->fetch_assoc()) {
       echo "<div class='row row-no-gutters transcript-grid'>
-        <div class='col-xs-2'>" . $row_transcript['subject_abbreviation'] . " " . $row_transcript['course_number'] . "</div>
+        <div class='col-xs-3'>" . $row_transcript['subject_abbreviation'] . " " . $row_transcript['course_number'] . "</div>
         <div class='col-xs-4'>" . $row_transcript['course_title'] . "</div>
-        <div class='col-xs-1'>" . $row_transcript['credit_hours'] . "</div>
+        <div class='col-xs-2'>" . $row_transcript['credit_hours'] . "</div>
         <div class='col-xs-2 mobile-hide'>" . $row_transcript['level_name'] . "</div>
         <div class='col-xs-1'>" . $row_transcript['letter_grade'] . "</div>
-        <div class='col-xs-2 mobile-hide'>" . $row_transcript['quality_points'] . "</div>
       </div>";
     }
     ?>
