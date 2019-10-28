cb60139607038071b012503b17b441dc7292745e
carsoncalcutt <calcutt.carson@gmail.com>
2019-10-28 10:24:29 -0400

    Create financial aid officer dashboard webpages

---
+++ financial-aid-officer/requirement-assignment-dashboard.php
@@ -0,0 +1,28 @@
+<?php
+include '../assets/financial-aid-officer/connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Requirement Assignment Dashboard</title>
+  <?php include '../assets/header.php'; ?>
+  <script type="text/javascript">
+  $(document).ready(function(){
+    $("#manage-requirements").addClass("active");
+  });
+  </script>
+</head>
+
+<body>
+
+  <?php include '../assets/financial-aid-officer/navbar.php'; ?>
+
+  <div class="container">
+
+    <h1>Requirement Assignment Dashboard</h1>
+
+  </div>
+</body>
+</html>
