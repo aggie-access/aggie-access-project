758a0eba3796ee34f37ede11b43d24617cbb3bca
carsoncalcutt <calcutt.carson@gmail.com>
2019-11-16 04:45:17 -0500

    Implement financial aid officer award management webpage with database

--- financial-aid-officer/award-management.php
+++ financial-aid-officer/award-management.php
@@ -1,5 +1,8 @@
 <?php
 include '../assets/financial-aid-officer/connect.php';
+
+$sql_funds = "SELECT * FROM fund ORDER BY fund_title ASC";
+$result_funds = $conn->query($sql_funds);
 ?>
 
 <!DOCTYPE html>
@@ -45,12 +48,22 @@ include '../assets/financial-aid-officer/connect.php';
             </div>
             <div class='modal-footer'>
               <button type='submit' class='btn btn-success'>Add Award</button>
+              <input type='hidden' name='addFund'>
               <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
             </div>
           </div>
         </div>
       </div>
     </form>
+
+    <?php
+    if(isset($_POST['addFund'])) {
+      $fund=$_POST['fund'];
+      $sql_create = "INSERT INTO fund (fund_title) VALUES ('$fund')";
+      $conn->query($sql_create);
+      echo "<meta http-equiv='refresh' content='0'>";
+    }
+    ?>
 
     <h1>Award Management</h1>
     <p style='margin-bottom:35px;'>You can add, edit, and delete financial aid awards from the system using the appropriate buttons below.</p>
@@ -63,173 +76,85 @@ include '../assets/financial-aid-officer/connect.php';
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Federal Pell Grant</td>
+
+        <?php
+        while($row_funds = $result_funds->fetch_assoc()) {
+          echo "<tr>
+          <td>" . $row_funds['fund_title'] . "</td>
           <td>
-            <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#editAward1'>Edit</button>
-            <button type='button' class='btn btn-danger' data-toggle='modal' data-target='#removeAward1'>Remove</button>
+          <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#editAward" . $row_funds['fund_id'] . "'>Edit</button>
+          <button type='button' class='btn btn-danger' data-toggle='modal' data-target='#removeAward" . $row_funds['fund_id'] . "'>Remove</button>
           </td>
-        </tr>
+          </tr>
 
-        <form action='' method='post'>
-          <div class='modal fade' id='editAward1' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-            <div class='modal-dialog'>
-              <div class='modal-content'>
-                <div class='modal-header'>
-                  <h4 class='modal-title'>Edit New Financial Aid Award</h4>
-                </div>
-                <div class='modal-body'>
-                  <p style='margin-bottom:25px;'>Enter your edits below for the selected financial aid award:</p>
-                  <div class='form-group'>
-                    <label>Fund Title</label>
-                    <input type='text' class='form-control' name='fund' value='Federal Pell Grant' required>
-                  </div>
-                </div>
-                <div class='modal-footer'>
-                  <button type='submit' class='btn btn-primary'>Submit</button>
-                  <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
-                </div>
-              </div>
-            </div>
+          <form action='' method='post'>
+          <div class='modal fade' id='editAward" . $row_funds['fund_id'] . "' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
+          <div class='modal-dialog'>
+          <div class='modal-content'>
+          <div class='modal-header'>
+          <h4 class='modal-title'>Edit Financial Aid Award</h4>
           </div>
-        </form>
-
-        <form action='' method='post'>
-          <div class='modal fade' id='removeAward1' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-            <div class='modal-dialog'>
-              <div class='modal-content'>
-                <div class='modal-header'>
-                  <h4 class='modal-title'>Remove Financial Aid Award</h4>
-                </div>
-                <div class='modal-body'>
-                  <p style='margin-bottom:25px;'>Are you sure that you would like to remove the following financial aid award from the system?</p>
-                  <div class='modal-data'>
-                    <div class='row'>
-                      <div class='col-sm-3'><strong>Fund Title</strong></div>
-                      <div class='col-sm-9'>Federal Pell Grant</div>
-                    </div>
-                  </div>
-                </div>
-                <div class='modal-footer'>
-                  <button type='submit' class='btn btn-danger'>Remove Award</button>
-                  <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
-                </div>
-              </div>
-            </div>
+          <div class='modal-body'>
+          <p style='margin-bottom:25px;'>Enter your edits below for the selected financial aid award:</p>
+          <div class='form-group'>
+          <label>Fund Title</label>
+          <input type='text' class='form-control' name='fund' value='" . $row_funds['fund_title'] . "' required>
           </div>
-        </form>
-
-        <tr>
-          <td>Campus Based Tuition Fund</td>
-          <td>
-            <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#editAward2'>Edit</button>
-            <button type='button' class='btn btn-danger' data-toggle='modal' data-target='#removeAward2'>Remove</button>
-          </td>
-        </tr>
-
-        <form action='' method='post'>
-          <div class='modal fade' id='editAward2' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-            <div class='modal-dialog'>
-              <div class='modal-content'>
-                <div class='modal-header'>
-                  <h4 class='modal-title'>Edit New Financial Aid Award</h4>
-                </div>
-                <div class='modal-body'>
-                  <p style='margin-bottom:25px;'>Enter your edits below for the selected financial aid award:</p>
-                  <div class='form-group'>
-                    <label>Fund Title</label>
-                    <input type='text' class='form-control' name='fund' value='Campus Based Tuition Fund' required>
-                  </div>
-                </div>
-                <div class='modal-footer'>
-                  <button type='submit' class='btn btn-primary'>Submit</button>
-                  <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
-                </div>
-              </div>
-            </div>
           </div>
-        </form>
-
-        <form action='' method='post'>
-          <div class='modal fade' id='removeAward2' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-            <div class='modal-dialog'>
-              <div class='modal-content'>
-                <div class='modal-header'>
-                  <h4 class='modal-title'>Remove Financial Aid Award</h4>
-                </div>
-                <div class='modal-body'>
-                  <p style='margin-bottom:25px;'>Are you sure that you would like to remove the following financial aid award from the system?</p>
-                  <div class='modal-data'>
-                    <div class='row'>
-                      <div class='col-sm-3'><strong>Fund Title</strong></div>
-                      <div class='col-sm-9'>Campus Based Tuition Fund</div>
-                    </div>
-                  </div>
-                </div>
-                <div class='modal-footer'>
-                  <button type='submit' class='btn btn-danger'>Remove Award</button>
-                  <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
-                </div>
-              </div>
-            </div>
+          <div class='modal-footer'>
+          <input type='hidden' name='fund-id' value='" . $row_funds['fund_id'] . "'>
+          <button type='submit' class='btn btn-primary'>Submit</button>
+          <input type='hidden' name='editFund" . $row_funds['fund_id'] . "'>
+          <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
           </div>
-        </form>
-
-        <tr>
-          <td>UNC Need Based Grant</td>
-          <td>
-            <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#editAward3'>Edit</button>
-            <button type='button' class='btn btn-danger' data-toggle='modal' data-target='#removeAward3'>Remove</button>
-          </td>
-        </tr>
-
-        <form action='' method='post'>
-          <div class='modal fade' id='editAward3' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-            <div class='modal-dialog'>
-              <div class='modal-content'>
-                <div class='modal-header'>
-                  <h4 class='modal-title'>Edit New Financial Aid Award</h4>
-                </div>
-                <div class='modal-body'>
-                  <p style='margin-bottom:25px;'>Enter your edits below for the selected financial aid award:</p>
-                  <div class='form-group'>
-                    <label>Fund Title</label>
-                    <input type='text' class='form-control' name='fund' value='UNC Need Based Grant' required>
-                  </div>
-                </div>
-                <div class='modal-footer'>
-                  <button type='submit' class='btn btn-primary'>Submit</button>
-                  <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
-                </div>
-              </div>
-            </div>
           </div>
-        </form>
-
-        <form action='' method='post'>
-          <div class='modal fade' id='removeAward3' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
-            <div class='modal-dialog'>
-              <div class='modal-content'>
-                <div class='modal-header'>
-                  <h4 class='modal-title'>Remove Financial Aid Award</h4>
-                </div>
-                <div class='modal-body'>
-                  <p style='margin-bottom:25px;'>Are you sure that you would like to remove the following financial aid award from the system?</p>
-                  <div class='modal-data'>
-                    <div class='row'>
-                      <div class='col-sm-3'><strong>Fund Title</strong></div>
-                      <div class='col-sm-9'>UNC Need Based Grant</div>
-                    </div>
-                  </div>
-                </div>
-                <div class='modal-footer'>
-                  <button type='submit' class='btn btn-danger'>Remove Award</button>
-                  <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
-                </div>
-              </div>
-            </div>
           </div>
-        </form>
+          </div>
+          </form>";
+
+          if(isset($_POST["editFund" . $row_funds['fund_id']])) {
+            $fundID=$_POST['fund-id'];
+            $fundTitle=$_POST['fund'];
+            $sql_update = "UPDATE fund SET fund_title='$fundTitle' WHERE fund_id='$fundID'";
+            $conn->query($sql_update);
+            echo "<meta http-equiv='refresh' content='0'>";
+          }
+
+          echo "<form action='' method='post'>
+          <div class='modal fade' id='removeAward" . $row_funds['fund_id'] . "' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>
+          <div class='modal-dialog'>
+          <div class='modal-content'>
+          <div class='modal-header'>
+          <h4 class='modal-title'>Remove Financial Aid Award</h4>
+          </div>
+          <div class='modal-body'>
+          <p style='margin-bottom:25px;'>Are you sure that you would like to remove the following financial aid award from the system?</p>
+          <div class='modal-data'>
+          <div class='row'>
+          <div class='col-sm-3'><strong>Fund Title</strong></div>
+          <div class='col-sm-9'>" . $row_funds['fund_title'] . "</div>
+          </div>
+          </div>
+          </div>
+          <div class='modal-footer'>
+          <input type='hidden' name='fund-id' value='" . $row_funds['fund_id'] . "'>
+          <button type='submit' class='btn btn-danger'>Remove Award</button>
+          <input type='hidden' name='removeFund" . $row_funds['fund_id'] . "'>
+          <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
+          </div>
+          </div>
+          </div>
+          </div>
+          </form>";
+
+          if(isset($_POST["removeFund" . $row_funds['fund_id']])) {
+            $fundID=$_POST['fund-id'];
+            $sql_delete = "DELETE FROM fund WHERE fund_id='$fundID'";
+            $conn->query($sql_delete);
+            echo "<meta http-equiv='refresh' content='0'>";
+          }
+        }
+        ?>
 
       </tbody>
     </table>
