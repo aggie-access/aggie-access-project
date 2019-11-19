f9d319ec1c621545f07d7936191572e2eb6a256b
Carson Calcutt <46988440+carsoncalcutt@users.noreply.github.com>
2019-11-19 10:34:52 -0500

    Update header.php

--- assets/header.php
+++ assets/header.php
@@ -5,4 +5,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css?v=<?php echo date('U'); ?>">
+<link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css">
